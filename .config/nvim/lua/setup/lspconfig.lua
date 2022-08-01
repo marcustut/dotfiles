471b3f041217b0047dbd56a16b8dd2efe6d88cc5
Marcus Lee <marcustutorial@hotmail.com>
2022-08-01 11:28:31 +0800

    updated fishconfig and vimrc

--- .config/nvim/lua/setup/lspconfig.lua
+++
@@ -1,9 +0,0 @@
-local lsp = require('lspconfig')
-
-lsp.pyright.setup(require('languages.python').lsp)
-lsp.volar.setup(require('languages.vue').lsp)
-lsp.jsonls.setup(require('languages.json').lsp)
-lsp.eslint.setup(require('languages.eslint').lsp)
-lsp.sumneko_lua.setup(require('languages.lua').lsp)
-lsp.rust_analyzer.setup(require('languages.rust').lsp)
-lsp.gopls.setup(require('languages.go').lsp)
