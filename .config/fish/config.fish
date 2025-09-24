eba72ce23b3e325f81ed49aaf1ac12100b862267
Marcus Lee <marcustutorial@hotmail.com>
2025-09-25 02:17:42 +0800

    feat: updated fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,64 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+    ################################
+    #                              #
+    #           Keymode            #
+    #                              #
+    ################################
+    fish_vi_key_bindings # vi mode (default insert)
+
+    ################################
+    #                              #
+    #           Aliases            #
+    #                              #
+    ################################
+    alias k="kubectl"
+    alias kns="kubens"
+    alias kctx="kubectx"
+    alias vim="nvim"
+    alias g="git"
+    alias grep="rg"
+    alias mkdir="mkdir -p"
+    alias cp="cp -v"
+    alias mv="mv -v"
+    alias rm="rm -v"
+    alias ls="eza --icons"
+    alias lla="eza -lah --icons"
+    alias cat="bat --paging=never --style=plain"
+
+    ################################
+    #                              #
+    #        Abbreviations         #
+    #                              #
+    ################################
+    abbr --add reload source ~/.config/fish/config.fish
+
+    # cargo
+    abbr --add cb cargo build
+    abbr --add cc cargo check
+    abbr --add cdo cargo doc --open
+    abbr --add cr cargo run
+
+    # git
+    abbr --add gaa git add -A
+    abbr --add ga git add
+    abbr --add gbd git branch --delete
+    abbr --add gb git branch
+    abbr --add gc git commit
+    abbr --add gcm git commit -m
+    abbr --add gcob git checkout -b
+    abbr --add gco git checkout
+    abbr --add gd git diff
+    abbr --add gl git log
+    abbr --add gp git push
+    abbr --add gpom git push origin main
+    abbr --add gs git status
+    abbr --add gst git stash
+    abbr --add gstp git stash pop
+
+    ################################
+    #                              #
+    #         Environment          #
+    #                              #
+    ################################
+    set -Ua fish_user_paths $HOME/.cargo/bin
 end
